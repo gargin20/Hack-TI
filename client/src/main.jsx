29e64e28b12a320bf8b46350f080bcd78c355187
Anjali <k.anjaliii.1011@gmail.com>
2026-06-18 22:51:16 +0530

    rollback to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,31 +5,7 @@ import './index.css'
 import store from './store/index.js'
 import AuthBootstrap from './components/AuthBootstrap.jsx'
 
-console.log('[BOOT] main.jsx evaluated', {
-  href: window.location.href,
-  rootExists: Boolean(document.getElementById('root')),
-});
-
-window.addEventListener('error', (event) => {
-  console.error('[BOOT] window error', {
-    message: event.message,
-    source: event.filename,
-    line: event.lineno,
-    column: event.colno,
-    error: event.error,
-  });
-});
-
-window.addEventListener('unhandledrejection', (event) => {
-  console.error('[BOOT] unhandled rejection', event.reason);
-});
-
-const rootElement = document.getElementById('root');
-console.log('[BOOT] createRoot about to run', { rootElement });
-
-
-
-createRoot(rootElement).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <AuthBootstrap />
